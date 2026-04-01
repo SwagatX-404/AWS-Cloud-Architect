c89fea684ac14c130884c0aef2ffaf8736ca46bd
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-04-01 20:19:06 +0530

    ref: cle

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import { Button } from "@mui/material";
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-green-600'>heloowowowo</h1>
-<div class="m-4 p-4 bg-yellow-200 font-bold rounded-lg">
-  <p>Tailwind csss working fine .</p>
-</div>
- <div className="flex items-center justify-center h-screen">
-      <Button variant="contained">Hello MUI</Button>
-    </div>
-    </div>
+    <div>App</div>
   )
 }
 
