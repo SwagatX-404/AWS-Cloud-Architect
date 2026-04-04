24dec561931abee802ffc1b37d7cd22341c33a86
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-04-04 20:45:35 +0530

    refactor

--- src/Page/Navbar/Navbar.jsx
+++ src/Page/Navbar/Navbar.jsx
@@ -2,19 +2,29 @@ import React from 'react'
 import { Avatar} from '@mui/material'
 import './Navbar.css'
 
+const APP_TITLE = 'Swg Task Manager'
+const USER_NAME = 'DevBySwg'
+const USER_INITIAL = 'S'
+
 const Navbar = () => {
   return (
-    <div className='container z-10 sticky left-0 right-0 top-0 py-3 px-5
-    lg:px-10 flex items-center justify-between'>
-       
-        <p className='font-bold text-lg'>Swg Task manager</p>
-
-        <div className='flex items-center gap-5'>
-            <p>DevBySwg</p>
-            <Avatar src=''>S</Avatar>
-        </div>
-    </div>
+    <nav className='container z-10 sticky left-0 right-0 top-0 py-3 px-5 lg:px-10 flex items-center justify-between'>
+      <AppTitle />
+      <UserInfo />
+    </nav>
   )
 }
+
+
+const AppTitle = () => (
+  <p className='font-bold text-lg'>{APP_TITLE}</p>
+)
+
+const UserInfo = () => (
+  <div className='flex items-center gap-5'>
+    <p>{USER_NAME}</p>
+    <Avatar src='' alt={USER_INITIAL}>{USER_INITIAL}</Avatar>
+  </div>
+)
 
 export default Navbar
