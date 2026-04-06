67c9e5aab60625287e4deffe5b003aefd3b7a512
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-04-06 19:22:13 +0530

    feat: sidebar avatar

--- src/Page/Sidebar/Sidebar.jsx
+++ src/Page/Sidebar/Sidebar.jsx
@@ -1,7 +1,17 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
+import './Sidebar.css'
 
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='card min-h-[85vh] flex flex-col justify-center fixed w-[20vw]'>
+      <div className='space-y-5 h-full'>
+        <div className='flex justify-center'>
+          <Avatar sx={{ width: "8rem", height: "8rem" }}
+          className='border-2 border-violet-600'
+            src='' />
+        </div>
+      </div>
+    </div>
   )
 }
