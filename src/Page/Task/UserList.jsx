a8177adc2946946409f8e6e2549a6c4da77fe9e7
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-04-22 21:52:35 +0530

    feat: user list

--- src/Page/Task/UserList.jsx
+++ src/Page/Task/UserList.jsx
@@ -28,11 +28,9 @@ export default function UserList({handleClose,open}) {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            User List
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+         
         </Box>
       </Modal>
     </div>
