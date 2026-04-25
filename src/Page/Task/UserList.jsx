75111d53417dda63180b54d7707e6d18cf2b1a80
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-04-25 18:34:16 +0530

    feat: avatar, list

--- src/Page/Task/UserList.jsx
+++ src/Page/Task/UserList.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Avatar, Divider, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 
 const style = {
   position: 'absolute',
@@ -11,10 +12,12 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  outline:"none",
   boxShadow: 24,
-  p: 4,
+  p: 2,
 };
+
+const tasks=[1,1,1,1]
 
 export default function UserList({ handleClose, open }) {
 
@@ -27,9 +30,29 @@ export default function UserList({ handleClose, open }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            User List
-          </Typography>
+         {
+          tasks.map((item,index)=>
+            <>
+            <div className='flex items-center justify-between w-full'>
+            <div>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar src=''/>
+                </ListItemAvatar>
+
+                <ListItemText secondary="@code_with_swg" primary={"Code with Swg"}/>
+              </ListItem>
+            </div>
+            <div>
+              <button className='customeButton'>select</button>
+            </div>
+
+          </div>
+            {index!==tasks.length-1 && <Divider variant='inset'/>}
+            </>
+          
+          )
+         }
 
         </Box>
       </Modal>
